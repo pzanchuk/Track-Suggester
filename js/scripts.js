8cc75447ab1742b032f5ff82268b905cbe26afcb
Pavel Zanchuk <buzzik@yahoo.com>
2019-01-18 15:52:57 -0800

    Add if/else statements, ass styles

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,36 @@ $(document).ready(function() {
 
     var inputName = $("input#name").val();
     var inputCity = $("input#city").val();
-    var interest = $("input:radio[name=interest]:checked").val();
+    var interest = parseInt($("input:radio[name=interest]:checked").val());
     var dislike = parseInt($("#dislike").val());
     var experience = $("#experience").val();
 
+    $(".result").show();
+    $(".result2").show();
+    $(".city").text(inputCity);
+    $(".name").text(inputName);
 
-    if(interest === "1"){
-      alert("correct");
+
+
+    if(experience === "1"){
+      $(".experience").text("do");
+    }else if(experience === "2"){
+      $(".experience").text("don't");
     }else{
-      alert("incorrect");
+      $(".result2").hide();
     }
 
 
-    $(".city").text(inputCity);
-    $(".name").text(inputName);
+
+    if(interest === 1 && dislike === 2 || interest === 1 && dislike === 3){
+      $(".language").text("PHP along with CSS");
+    }else if( interest === 2 && dislike === 1 || interest === 2 && dislike === 3){
+      $(".language").text("C# and .NET");
+    }else if(interest === 3 && dislike === 2 || interest === 3 && dislike === 3){
+      $(".language").text("HTML5, PHP, JQuery");
+    }else{
+      $(".language").text("Java and Android");
+    }
 
       event.preventDefault();
   });
